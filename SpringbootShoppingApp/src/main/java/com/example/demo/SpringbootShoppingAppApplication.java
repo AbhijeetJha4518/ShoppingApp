d30a0e042b7a0c2ccb93ee4e44105b606873b000
Abhijeet <Abhijeet.Jha4@cognizant.com>
2025-01-22 11:08:26 +0530

    Initial commit

---
+++ SpringbootShoppingApp/src/main/java/com/example/demo/SpringbootShoppingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootShoppingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootShoppingAppApplication.class, args);
+	}
+
+}
