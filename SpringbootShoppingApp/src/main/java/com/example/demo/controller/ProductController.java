5dc55bab80952528f2a603f4c5dcfac0d8993032
Abhijeet <Abhijeet.Jha4@cognizant.com>
2025-01-24 11:20:26 +0530

    Logger added

--- SpringbootShoppingApp/src/main/java/com/example/demo/controller/ProductController.java
+++ SpringbootShoppingApp/src/main/java/com/example/demo/controller/ProductController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,11 +15,21 @@ import com.example.demo.service.ProductService;
 @RequestMapping("/products") //http://localhost:2001/products
 public class ProductController {
 
-	@Autowired
-	ProductService service;
-	
-	@PostMapping("/save")  //http://localhost:2001/products/save
-	public String saveProduct(@RequestBody Product product) {
-		return service.addProduct(product);
-	}
+    private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
+
+    @Autowired
+    ProductService service;
+    
+    @PostMapping("/save")  //http://localhost:2001/products/save
+    public String saveProduct(@RequestBody Product product) {
+        logger.info("Received request to save product: {}", product);
+        try {
+            String response = service.addProduct(product);
+            logger.info("Product saved successfully: {}");
+            return response;
+        } catch (Exception e) {
+            logger.error("Error occurred while saving product: {}",e);
+            return "Error saving product";
+        }
+    }
 }
